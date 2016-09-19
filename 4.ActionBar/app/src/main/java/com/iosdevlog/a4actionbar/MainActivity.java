a0c444b4d600d26d7f3aa5b335acc9863c918bf9
iosdevlog <iosdevlog@iosdevlog.com>
2016-09-19 11:22:03 +0800

    4. ActionBar

---
+++ 4.ActionBar/app/src/main/java/com/iosdevlog/a4actionbar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.iosdevlog.a4actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 为ActionBar扩展菜单项
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 处理动作按钮的点击事件
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openSearch() {
+        Log.d(TAG, "openSearch: ");
+    }
+
+    private void openSettings() {
+        Log.d(TAG, "openSettings: ");
+    }
+}
