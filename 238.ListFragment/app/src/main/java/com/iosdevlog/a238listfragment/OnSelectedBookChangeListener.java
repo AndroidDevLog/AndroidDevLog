cd90c28565fb4b87ecd7894d67fc229b6f2055d0
iosdevlog <iosdevlog@iosdevlog.com>
2016-12-08 14:21:24 +0800

    BookListFragment

---
+++ 238.ListFragment/app/src/main/java/com/iosdevlog/a238listfragment/OnSelectedBookChangeListener.java
@@ -0,0 +1,8 @@
+package com.iosdevlog.a238listfragment;
+
+/**
+ * Created by Jim on 2/5/2016.
+ */
+public interface OnSelectedBookChangeListener {
+    void onSelectedBookChanged(int bookIndex);
+}
