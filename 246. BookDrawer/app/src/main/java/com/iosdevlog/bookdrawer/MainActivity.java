40797bfef45a5e106438df00614ddc1c9ba3133b
iosdevlog <iosdevlog@iosdevlog.com>
2016-12-26 17:48:09 +0800

    246. Drawer

---
+++ 246. BookDrawer/app/src/main/java/com/iosdevlog/bookdrawer/MainActivity.java
@@ -0,0 +1,120 @@
+package com.iosdevlog.bookdrawer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    String[] mTitlesShort;
+    String[] mDescriptions;
+    int[] mTopImageResourceIds = {
+            R.drawable.db_programming_top_card,
+            R.drawable.android_4_top_card,
+            R.drawable.sys_dev_top_card,
+            R.drawable.and_engine_top_card
+    };
+
+    TextView bookTitleView;
+    TextView bookDescriptionView;
+    ImageView topImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bookTitleView = (TextView)findViewById(R.id.bookTitle);
+        bookDescriptionView = (TextView)findViewById(R.id.bookDescription);
+        topImageView = (ImageView)findViewById(R.id.topImage);
+
+        mTitlesShort = getResources().getStringArray(R.array.book_titles_short);
+        mDescriptions = getResources().getStringArray(R.array.book_descriptions);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    void setContent(int index) {
+        bookTitleView.setText(mTitlesShort[index]);
+        bookDescriptionView.setText(mDescriptions[index]);
+        topImageView.setImageResource(mTopImageResourceIds[index]);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            setContent(0);
+        } else if (id == R.id.nav_gallery) {
+            setContent(1);
+        } else if (id == R.id.nav_slideshow) {
+            setContent(2);
+        } else if (id == R.id.nav_manage) {
+            setContent(3);
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
